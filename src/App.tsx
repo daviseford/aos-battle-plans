5dc38107ea207a66114686ea9cba355a45032529
Davis Ford <davis.e.ford@gmail.com>
2020-01-01 19:42:44 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import React from 'react'
-import { Stage, Layer, Rect, Text } from 'react-konva'
+import React, { useState } from 'react'
+import { Stage, Layer, Text, Circle } from 'react-konva'
 import Konva from 'konva'
 import './App.css'
 
-class ColoredRect extends React.Component {
-  state = {
-    color: 'green',
-  }
-  handleClick = () => {
-    this.setState({
-      color: Konva.Util.getRandomColor(),
-    })
-  }
-  render() {
-    return (
-      <Rect
-        x={20}
-        y={20}
-        width={50}
-        height={50}
-        fill={this.state.color}
-        shadowBlur={5}
-        onClick={this.handleClick}
-      />
-    )
-  }
+const ColoredCircle = () => {
+  const [color, setColor] = useState('green')
+
+  console.log(Konva.Util.getRandomColor())
+
+  return (
+    <Circle
+      x={30}
+      y={50}
+      radius={15}
+      fill={color}
+      shadowBlur={5}
+      onClick={() => setColor(Konva.Util.getRandomColor())}
+      draggable={true}
+    />
+  )
 }
 
 const App = () => {
@@ -34,8 +28,10 @@ const App = () => {
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
-        <Text text="Try click on rect" />
-        <ColoredRect />
+        <Text text="For Darren Watson's eyes only" offsetX={-20} offsetY={-20} />
+        {[...Array(5)].map((x, i) => (
+          <ColoredCircle key={i} />
+        ))}
       </Layer>
     </Stage>
   )
