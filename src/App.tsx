8ba166860d7ebfa8492888f7d703994da4bedbf9
Davis Ford <davis.e.ford@gmail.com>
2020-01-06 18:37:41 -0500

    Adjust some sizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Stage, Layer, Text, Group } from 'react-konva'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import CircleBase from 'components/circleBase'
 
 const App = () => {
@@ -8,20 +9,32 @@ const App = () => {
   // Layer is actual canvas element (so you may have several canvases in the stage)
   // And then we have canvas shapes inside the Layer
 
+  // Inches to mm
+  const [tableX, tableY] = [72, 48]
+  const canvasX = window.innerWidth
+  const conversionPercentX = tableX / canvasX
+
+  const canvasY = canvasX * (tableY / tableX)
+
   const radius = 12.5
   const xSpacing = radius * 3
 
+  debugger
+
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Text text="For Darren Watson's eyes only" offsetX={-20} offsetY={-20} />
-        <Group draggable={true}>
-          {[...Array(5)].map((x, i) => (
-            <CircleBase key={i} x={30 + xSpacing * i} y={50} radius={radius} />
-          ))}
-        </Group>
-      </Layer>
-    </Stage>
+    <div className="" style={{ backgroundColor: 'black', paddingBottom: '50px' }}>
+      <Stage width={canvasX} height={canvasY} style={{ backgroundColor: 'white' }}>
+        <Layer>
+          <Text text="For Darren Watson's eyes only" offsetX={-20} offsetY={-20} />
+          <Group draggable={true}>
+            {[...Array(5)].map((x, i) => (
+              <CircleBase key={i} x={30 + xSpacing * i} y={50} radius={radius} />
+            ))}
+          </Group>
+        </Layer>
+      </Stage>
+      Bottom of Stage
+    </div>
   )
 }
 
