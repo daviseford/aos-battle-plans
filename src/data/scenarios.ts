b86c0336481c404001595cce4b0465124e541970
Davis Ford <davis.e.ford@gmail.com>
2020-01-06 22:03:26 -0500

    Update scenarios.ts

--- src/data/scenarios.ts
+++ src/data/scenarios.ts
@@ -4,22 +4,52 @@ export interface ISetupRestrictions {
   fromBoardEgeInches: number | null
 }
 
+export interface IObjective {
+  x: number
+  y: number
+}
+
 export interface IScenario {
   name: string
   matchedPlay: boolean
   orientation: 'horizontal' | 'vertical' | 'diagonalTopLeft' | 'diagonalTopRight'
   setupRestrictions: ISetupRestrictions
+  // Measured in inches, [x, y]
+  objectives: IObjective[]
 }
 
 const Scenarios: IScenario[] = [
   {
-    name: `Scorched Earth`,
+    name: `Total Commitment`,
     matchedPlay: true,
     orientation: `horizontal`,
     setupRestrictions: {
       fromDividerInches: 9,
       fromBoardEgeInches: 12,
     },
+    objectives: [
+      { x: 12, y: 12 },
+      { x: 60, y: 12 },
+      { x: 12, y: 36 },
+      { x: 60, y: 36 },
+    ],
+  },
+  {
+    name: `Scorched Earth`,
+    matchedPlay: true,
+    orientation: `horizontal`,
+    setupRestrictions: {
+      fromDividerInches: 9,
+      fromBoardEgeInches: null,
+    },
+    objectives: [
+      { x: 12, y: 12 },
+      { x: 36, y: 12 },
+      { x: 60, y: 12 },
+      { x: 12, y: 36 },
+      { x: 36, y: 36 },
+      { x: 60, y: 36 },
+    ],
   },
 ]
 
